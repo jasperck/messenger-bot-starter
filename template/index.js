4f26e091fd76a71128e81f09cad26cf6f745b59f
Jasper Chang <jasperc8@gmail.com>
2017-02-18 09:45:27 +0800

    Add message and attachment templates

---
+++ template/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  audio: require('./audio'),
+  image: require('./image'),
+  video: require('./video'),
+  button: require('./button'),
+  generic: require('./generic'),
+  list: require('./list'),
+  call: require('./call'),
+  quickReply: require('./quick-reply')
+};
